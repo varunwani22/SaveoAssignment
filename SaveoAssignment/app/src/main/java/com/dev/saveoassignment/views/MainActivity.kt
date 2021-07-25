b818cf1b85c43403a5efbe93d402ba68be2b5702
Varun Wani <varunwani22@gmail.com>
2021-07-25 11:13:49 +0530

    Initial Setup

---
+++ SaveoAssignment/app/src/main/java/com/dev/saveoassignment/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dev.saveoassignment.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.saveoassignment.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
